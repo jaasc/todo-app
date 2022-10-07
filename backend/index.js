3d950fa00fbf8383cbd9f98e2f3990de4c980461
Jasmine <104405058+jaasc@users.noreply.github.com>
2022-10-07 17:32:42 +0800

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,11 @@ require('dotenv').config()
 const connection_string = process.env.CONNECTION_STRING;
 const port = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors({
+    origin: "*",
+    credentials: true,
+}));
+
 app.use(express.json());
 app.use('/', todos);
 app.use('/signup', signUp);
